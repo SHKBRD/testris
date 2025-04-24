b0f6037d0f05d7cd276b1e71048145c4b7b469be
SHKBRD <121646151+SHKBRD@users.noreply.github.com>
2025-04-24 13:22:22 -0400

    add invalid kick protection

--- tetris.lua
+++ tetris.lua
@@ -424,20 +424,62 @@ end
 function attempt_rotate_tetrimino(dir, tetri)
     check_tetrimino = {}
     check_tetrimino = deepcopy(tetri, {})
+    can_rotate = true
+    checking_kick = true
+    if check_tetrimino.pieceid <= 5 and check_tetrimino.pieceid >= 3 and (check_tetrimino.rotation_ind%2)==1 then
+        --stop(check_tetrimino.rotation_ind)
+        for row=1,#check_tetrimino.piecegrid do
+            for col=1,#check_tetrimino.piecegrid[row] do
+                block = check_tetrimino.piecegrid[row][col]
+                --stop(block)
+                if block == 0 then
+                    --stop((row-1)*3+col)
+                    boardpx = check_tetrimino.x+col-1
+                    boardpy = check_tetrimino.y+row-1
+                    -- if (boardpx < 1 or boardpx>boardsizex) and block != 0 then
+                    --     --stop(boardpx)
+                    --     return true
+                    -- end
+                    -- if (boardpy < 1 or boardpy>boardsizey) and block != 0 then
+                    --     --stop(boardpy)
+                    --     return true
+                    -- end
+                    
+                    if board[boardpy][boardpx].issolid and col == 2 then
+                        --stop(2)
+                        can_rotate = false
+                        checking_kick = false
+                    end
+                    
+                end
+                if (not checking_kick) break
+            end
+            if (not checking_kick) break
+        end
+    end
+    checking_kick = false
+    if not can_rotate then 
+        stop("no kick!")
+        return
+    end
+
     rotate_tetrimino(dir, check_tetrimino)
     if not is_piece_colliding_grid(check_tetrimino) then
         currpiece = check_tetrimino
         return true
     end
-    check_tetrimino.x -= 1
-    if not is_piece_colliding_grid(check_tetrimino) then
-        currpiece = check_tetrimino
-        return true
-    end
-    check_tetrimino.x += 2
-    if not is_piece_colliding_grid(check_tetrimino) then
-        currpiece = check_tetrimino
-        return true
+    --don't check wallkicks on 
+    if check_tetrimino.pieceid != 1 then
+        check_tetrimino.x -= 1
+        if not is_piece_colliding_grid(check_tetrimino) then
+            currpiece = check_tetrimino
+            return true
+        end
+        check_tetrimino.x += 2
+        if not is_piece_colliding_grid(check_tetrimino) then
+            currpiece = check_tetrimino
+            return true
+        end
     end
     return false
 end
